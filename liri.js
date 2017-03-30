dc2c517b332cf267e06dd78cda311aa4ff6477cb
Tom Langton <tom@pulpinfo.com>
2017-03-30 12:41:37 -0400

    Movie logging cleaned up.

--- liri.js
+++ liri.js
@@ -9,13 +9,13 @@ var fs = require("fs");
 var request = require("request");
 var moment = require("moment");
 var aLine = "-------------------------------------------\n\n";
-
+var row40 = "----------------------------------------"; //40 hyphens!
+var row80 =  (row40 + row40).slice(-80);
+var returns2 = "\n\n";
+var returns1 = "\n";
 //////////////////////
 // Variable declaration
 //////////////////////
-
-sString = String(10, "M")
-console.log(sString + '\n');
 
 //the intro text
 var introText = "Who approaches the Bridge of Death must answer me these questions three, 'ere the other side he see?";
@@ -32,33 +32,33 @@ var rTyp;
 /////////////////////////////////////////
 
 var questions = [
-{
-    type: "list",
-    message: "What Request type?",
-    choices: ["Twitter", "Spotify", "Movie", "Random"],
-    name: "requestType"
-},
-{
+    {
+        type: "list",
+        message: "What Request type?",
+        choices: ["Twitter", "Spotify", "Movie", "Random"],
+        name: "requestType"
+    },
+    {
         //title is requested only if spotify or movie is specified
         type: "input",
         message: "Movie or Song Title?",
         name: "requestText",
         when: function(answers) {
             return answers.requestType === "Spotify" ||
-            answers.requestType === "Movie";
+                answers.requestType === "Movie";
         }
     }
-    ];
+];
 
 // Create a "Prompt" with a series of questions from var above.
 inquirer
-.prompt(
+    .prompt(
         // Here we give the user a list to choose from.
         questions // the variable
         // Once we are done with all the questions... "then" we do stuff with the answers
         // In this case, we store all of the answers into a "user" object that inquirer makes for us.
-        )
-.then(function(user) {
+    )
+    .then(function(user) {
         // If we log that user as a JSON, we can see how it looks.
         // console.log(JSON.stringify(user, null, 2));
 
@@ -67,34 +67,38 @@ inquirer
 
         switch (rType) {
             case "Spotify":
-            spotify(rText);
-            break;
+                spotify(rText);
+                break;
 
             case "Twitter":
-            tweets();
-            break;
+                tweets();
+                break;
 
             case "Movie":
-            movie(rText);
-            break;
+                movie(rText);
+                break;
 
             case "Random":
-            selfDetermined();
-            break;
+                selfDetermined();
+                break;
 
             default:
-            ("unknown choice");
-            break;
+                ("unknown choice");
+                break;
         }
 
         function writeLog(data) {
             // var tNow = moment().format("MM DD YYYY, h:mm:ss a");
             var tNow = new Date();
-            fs.appendFile("log.txt", data + " Logged: " + tNow + "\n", function(err) {
-                if (err) throw err;
+            fs.appendFile(
+                "log.txt",
+                data + " Logged: " + tNow + "\n" + row80 + returns1,
+                function(err) {
+                    if (err) throw err;
 
-                // console.log("Saved!");
-            });
+                    // console.log("Saved!");
+                }
+            );
         }
 
         // Based on requestType run the appropriate function
@@ -120,8 +124,8 @@ inquirer
         ////////////////////////
         function movie(title) {
             var queryURL = "http://www.omdbapi.com/?t=" +
-            title +
-            "&y=&plot=short&tomatoes=true&r=json";
+                title +
+                "&y=&plot=short&tomatoes=true&r=json";
 
             request(queryURL, function(error, response, body) {
                 // If the request is successful (i.e. if the response status code is 200)
@@ -140,40 +144,48 @@ inquirer
                         // console.log("rating "+ rtRating);
                     }
 
-                    var movieLog = "\n\n---------- Title: " +
-                    jsBody.Title +
-                    " ---------- \n" +
-                    " Release Year: " +
-                    jsBody.Year +
-                    "\n" +
-                    " IMDB Rating: " +
-                    jsBody.imdbRating +
-                    "\n" +
-                    " Produced in: " +
-                    jsBody.Country +
-                    "\n" +
-                    " Language: " +
-                    jsBody.Language +
-                    "\n" +
-                    " Plot: " +
-                    jsBody.Plot +
-                    "\n" +
-                    " Actors: " +
-                    jsBody.Actors +
-                    "\n" +
-                    " Rotten Tomatoes Rating: " +
-                    rtRating +
-                    "\n" +
-                    " Rotten Tomatoes URL: " +
-                    jsBody.tomatoURL +
-                    "\n";
+                    var tLength = jsBody.Title.length;
+                    // console.log("tLength "+ tLength);
 
+                    var cPadding = (71 - tLength) / 2;
+                    var cPaddingSlice = row40.slice(-cPadding);
+                    var titleString = returns2 + cPaddingSlice +
+                        " Title: " +
+                        jsBody.Title +
+                        " " +
+                        cPaddingSlice;
+                    // console.log(titleString);
+
+                    var movieLog = titleString +
+                        returns1 +
+                        " Release Year: " +
+                        jsBody.Year +
+                        "\n" +
+                        " IMDB Rating: " +
+                        jsBody.imdbRating +
+                        "\n" +
+                        " Produced in: " +
+                        jsBody.Country +
+                        "\n" +
+                        " Language: " +
+                        jsBody.Language +
+                        "\n" +
+                        " Plot: " +
+                        jsBody.Plot +
+                        "\n" +
+                        " Actors: " +
+                        jsBody.Actors +
+                        "\n" +
+                        " Rotten Tomatoes Rating: " +
+                        rtRating +
+                        "\n" +
+                        " Rotten Tomatoes URL: " +
+                        jsBody.tomatoURL +
+                        "\n";
                 }
 
-
-
-                console.log(movieLog,aLine);
-                writeLog(movieLog,aLine);
+                console.log(movieLog,row80,returns2);
+                writeLog(movieLog);
             });
         }
 
