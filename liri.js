54e2d3310961d28c44dee202fb664f468739876c
Tom Langton <tom@pulpinfo.com>
2017-03-31 16:58:27 -0400

    movies and spotify in a good place.

--- liri.js
+++ liri.js
@@ -8,37 +8,38 @@ var spotify = require("spotify");
 var fs = require("fs");
 var request = require("request");
 var moment = require("moment");
-var aLine = "-------------------------------------------\n\n";
-var row40 = "----------------------------------------"; //40 hyphens!
-var row80 = (row40 + row40).slice(-80);
-var returns2 = "\n\n";
-var returns1 = "\n";
+
 //////////////////////
 // Variable declaration
 //////////////////////
 
 //the intro text
-var introText = "Who approaches the Bridge of Death must answer me these questions three, 'ere the other side he see?";
-console.log(introText);
-
+var introText = "     Who approaches the Bridge of Death must answer me ";
+var introText2 = "       these questions three,'ere the other side he see:";
 var client = new Twitter(keyxpt.twitterKeys);
 var rText;
 var rTyp;
+var row40 = "----------------------------------------"; //40 hyphens!
+var row80 = (row40 + row40).slice(-80);
+var returns2 = "\n\n";
+var returns1 = "\n";
+var defaultTrack = "";
 
-// console.log(client);
 
+//Console header//////
+console.log(row80 + returns1 + introText + "\n" + introText2 + returns1 + row80);
 /////////////////////////////////////////
 // User Input   -- inquired questions  --
 /////////////////////////////////////////
 
 var questions = [
-{
-    type: "list",
-    message: "What Request type?",
-    choices: ["Twitter", "Spotify", "Movie", "Random"],
-    name: "requestType"
-},
-{
+    {
+        type: "list",
+        message: "What Request type?",
+        choices: ["Twitter", "Spotify", "Movie", "Random"],
+        name: "requestType"
+    },
+    {
         //movie title is requested if movie picked
         type: "input",
         message: "Movie Title?",
@@ -56,18 +57,17 @@ var questions = [
             return answers.requestType === "Spotify";
         }
     }
-
-    ];
+];
 
 // Create a "Prompt" with a series of questions from var above.
 inquirer
-.prompt(
+    .prompt(
         // Here we give the user a list to choose from.
         questions // the variable
         // Once we are done with all the questions... "then" we do stuff with the answers
         // In this case, we store all of the answers into a "user" object that inquirer makes for us.
-        )
-.then(function(user) {
+    )
+    .then(function(user) {
         // If we log that user as a JSON, we can see how it looks.
         // console.log(JSON.stringify(user, null, 2));
 
@@ -76,24 +76,24 @@ inquirer
 
         switch (rType) {
             case "Spotify":
-            spotify(rText);
-            break;
+                spotifyTrack(rText);
+                break;
 
             case "Twitter":
-            tweets();
-            break;
+                tweets();
+                break;
 
             case "Movie":
-            movie(rText);
-            break;
+                movie(rText);
+                break;
 
             case "Random":
-            selfDetermined();
-            break;
+                selfDetermined();
+                break;
 
             default:
-            ("unknown choice");
-            break;
+                ("unknown choice");
+                break;
         }
 
         function writeLog(data) {
@@ -104,10 +104,16 @@ inquirer
                 data + " Logged: " + tNow + "\n" + row80 + returns1,
                 function(err) {
                     if (err) throw err;
-
-                    // console.log("Saved!");
                 }
-                );
+            );
+        }
+
+        function writeLogLess(data) {
+            // var tNow = moment().format("MM DD YYYY, h:mm:ss a");
+
+            fs.appendFile("log.txt", data, function(err) {
+                if (err) throw err;
+            });
         }
 
         // Based on requestType run the appropriate function
@@ -123,19 +129,84 @@ inquirer
         ////////////////////////
         //// spotify
         ////////////////////////
-        var aOfB = "https://api.spotify.com/v1/search?q=track:the%20sign%20artist:ace%20of%20base&type=track"
+        var aOfB = "https://api.spotify.com/v1/search?q=track:the%20sign%20artist:ace%20of%20base&type=track";
+        var countReportTracks;
+        var reportTable;
+        // var spotifyLogHeader;
 
-        function spotify(title) {
-            console.log("spot: " + title);
-            writeLog("spot " + title);
+        function spotifyTrack(title) {
+            // console.log("spot: " + title);
+            // writeLog("spot " + title);
 
-            var queryURL = "https://api.spotify.com/v1/search?q=" + title + "&type=track";
-            request(queryURL, function(error, response, body) {
-                console.log(body);
-                return;
-            });
+             if (title == "") {
+                var title = "The Sign Ace of Base";
+                defaultTrack = "No search params! - Default Track -> ";
+            }
 
+            spotify.search(
+                { type: "track", query: title },
+                function(err, data) {
+                    if (err) {
+                        console.log("Error occurred: " + err);
+                        return;
+                    }
+                    var itemLength = data.tracks.items.length;
 
+                    if (itemLength > 4) {
+                        countReportTracks = 5;
+                    } else {
+                        countReportTracks = itemLength;
+                    }
+
+                    var spotifyLogHeader = returns1 + row80 +
+                        returns1 + defaultTrack +
+                        "Spotify search: " +
+                        title +
+                        "\n" +
+                        "Matches: " +
+                        itemLength +
+                        "\n" +
+                        "First " +
+                        countReportTracks +
+                        " Matches shown.\n" +
+                        row80 +
+                        returns1;
+
+                    writeLog(spotifyLogHeader);
+                    console.log(spotifyLogHeader);
+
+                    if (itemLength < 1) {
+                        // console.log("No matches.")
+                    } else {
+                        for (var i = 0; i < countReportTracks; i++) {
+                            var songName = data.tracks.items[i].name;
+                            var albumName = data.tracks.items[i].album.name;
+                            var songURL = data.tracks.items[i].album.artists[
+                                0
+                            ].external_urls.spotify;
+                            var songArtists = data.tracks.items[
+                                i
+                            ].album.artists[0].name;
+
+                            var logSong = "Song Title: " +
+                                songName +
+                                "  ---> Artist: " +
+                                songArtists +
+                                returns1 +
+                                "Album: " +
+                                albumName +
+                                returns1 +
+                                songURL +
+                                returns1 +
+                                row80 +
+                                returns1;
+
+                            console.log(logSong);
+                            writeLogLess(logSong);
+                        }
+                    }
+                }
+            );
         }
 
         ////////////////////////
@@ -145,17 +216,28 @@ inquirer
             //if no title entered, Nobody's the default.
             if (title == "") {
                 var title = "Mr. Nobody";
+                defaultTrack = "No film selected - default movie choice <->"
             }
 
             var queryURL = "http://www.omdbapi.com/?t=" +
-            title +
-            "&y=&plot=short&tomatoes=true&r=json";
+                title +
+                "&y=&plot=short&tomatoes=true&r=json";
 
             request(queryURL, function(error, response, body) {
                 // If the request is successful (i.e. if the response status code is 200)
                 if (!error && response.statusCode === 200) {
                     var jsBody = JSON.parse(body);
                     // console.log(jsBody);
+
+
+if (jsBody.Ratings === undefined) {
+var noMatchOmdb = (row80 + returns1 + "No Matches - Movie search on: "+ title + returns1 + row80 + returns1 );
+console.log(noMatchOmdb);
+writeLog(noMatchOmdb);
+    return;
+};
+
+
 
                     //Checking to see if Rotten Tomatoes rating exists
                     //if it exists, get the rating into a var
@@ -171,47 +253,49 @@ inquirer
                     //calculating header log line output - making it pretty.
                     //the title line and the last line will be 80 characters long, the title centered.
                     var tLength = jsBody.Title.length;
-                    var cPadding = (71 - tLength) / 2;
+                    var cPadding = (71 - tLength) / 2; //"71" = 80 columns less 9 chars for " Title: "
                     var cPaddingSlice = row40.slice(-cPadding);
                     var titleString = returns2 +
-                    cPaddingSlice +
-                    " Title: " +
-                    jsBody.Title +
-                    " " +
-                    cPaddingSlice;
+                        cPaddingSlice +
+                        " Title: " +
+                        jsBody.Title +
+                        " " +
+                        cPaddingSlice;
                     // console.log(titleString);
 
                     var movieLog = titleString +
-                    returns1 +
-                    " Release Year: " +
-                    jsBody.Year +
-                    "\n" +
-                    " IMDB Rating: " +
-                    jsBody.imdbRating +
-                    "\n" +
-                    " Produced in: " +
-                    jsBody.Country +
-                    "\n" +
-                    " Language: " +
-                    jsBody.Language +
-                    "\n" +
-                    " Plot: " +
-                    jsBody.Plot +
-                    "\n" +
-                    " Actors: " +
-                    jsBody.Actors +
-                    "\n" +
-                    " Rotten Tomatoes Rating: " +
-                    rtRating +
-                    "\n" +
-                    " Rotten Tomatoes URL: " +
-                    jsBody.tomatoURL +
-                    "\n";
+                        returns1 +
+                        defaultTrack +
+                        " Release Year: " +
+                        jsBody.Year +
+                        "\n" +
+                        " IMDB Rating: " +
+                        jsBody.imdbRating +
+                        "\n" +
+                        " Produced in: " +
+                        jsBody.Country +
+                        "\n" +
+                        " Language: " +
+                        jsBody.Language +
+                        "\n" +
+                        " Plot: " +
+                        jsBody.Plot +
+                        "\n" +
+                        " Actors: " +
+                        jsBody.Actors +
+                        "\n" +
+                        " Rotten Tomatoes Rating: " +
+                        rtRating +
+                        "\n" +
+                        " Rotten Tomatoes URL: " +
+                        jsBody.tomatoURL +
+                        "\n";
                 }
 
                 console.log(movieLog, row80, returns2);
                 writeLog(movieLog);
             });
+            writeLogLess(returns2);
         }
 
         ////////////////////////
